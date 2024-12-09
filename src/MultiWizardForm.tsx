47353153df158f2e922616f1a2598bc1792f26e7
zakariyyah shamsudeen <zakariyyahshamsudeen@gmail.com>
2024-12-09 12:36:08 +0100

    small updated

--- src/MultiWizardForm.tsx
+++ src/MultiWizardForm.tsx
@@ -5,7 +5,7 @@ import CareerForm from './CareerForm';
 
 const MultiStepForm: React.FC = () => {
   const [step, setStep] = useState(1);
-  const { formState } = useFormContext();
+  const { formState, username } = useFormContext();
 
   const handleNext = () => setStep((prev) => prev + 1);
   const handleBack = () => setStep((prev) => prev - 1);
@@ -15,7 +15,7 @@ const MultiStepForm: React.FC = () => {
 
   return (
     <div className="multi-step-form max-w-lg mx-auto p-6">
-      <h1>hello</h1>
+      <h1>hello {username}</h1>
       {step === 1 && <EducationForm onNext={handleNext} />}
       {step === 2 && <CareerForm onBack={handleBack} onSubmit={handleSubmit} />}
     </div>
